8c7f87900e86c96ce0e2a3895a83e98018c23fdc
Aaron Lockhart <aaronlockhartdev@gmail.com>
2020-01-23 22:29:42 -0500

    failed attempt

--- models/model.py
+++ models/model.py
@@ -1,4 +1,45 @@
-class Model():
-    def __init__(self):
+import tensorflow as tf
+from tensorflow.keras.layers import *
 
-    def __call__(self):
+class Model(tf.keras.Model):
+    def __init__(self, boardSize):
+        super().__init__()
+
+        self.conv2d = Conv2D(24, (int(boardSize/2), int(boardSize/2)), strides=(1, 1), padding='same')
+        self.concat = Concatenate()
+
+        self.dense1 = Dense(10, activation='relu')
+        self.dense2 = Dense(12, activation='relu')
+        self.dense3 = Dense(8, activation='relu')
+
+        self.lambda1 = Lambda(lambda x: (x[0][x[1]]))
+
+    def __call__(self, board, start, end, training=False):
+        b = self.conv2d(board)
+        b = self.concat([board, b])
+        print(b.shape)
+
+        current = start
+        moves = [[1, 1], [1, 0], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]]
+
+        index = tf.cast([start], tf.int32)
+        index = tf.squeeze(index)
+        print(index.shape)
+        s = self.lambda1((b, index))
+
+        while current[0] != end[0] or current[1] != end[1]:
+            gen = [[current[0] + moves[i][0], current[1] + moves[i][1]] for i in range(8)]
+            vals = tf.convert_to_tensor([b[gen[i][0]][gen[i][1]] for i in range(8)])
+            
+            x = self.dense1(vals)
+            x = self.dense2(x)
+            x = self.dense3(x)
+
+            chosen = tf.math.argmax(x)
+
+            current = gen[chosen]
+            s += vals[chosen]
+
+        return s
+
+        
--- preprocessing/process.py
+++ preprocessing/process.py
@@ -1,5 +1,16 @@
-class Processor():
-    def __init__(self):
+import numpy as np
 
-    def __call__(self):
+class Processor():
+    def __init__(self, batchSize):
+        self.batchSize = batchSize
         
+    def __call__(self):
+        boards = np.random.rand(100000, self.batchSize, 10, 10, 1)
+        starts = np.random.rand(100000, self.batchSize, 2)
+        starts = starts * 10
+        starts = starts.astype(np.int)
+        ends = np.random.rand(100000, self.batchSize, 2)
+        ends = ends * 10
+        ends = ends.astype(np.int)
+
+        return boards, starts, ends
